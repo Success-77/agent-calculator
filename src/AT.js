8675e0445bce87ba967bf50ec22c1dd61def7f94
Abdul Rahman Benyi <afayidyllic@gmail.com>
2024-04-02 15:04:49 +0000

    UI and app extension Done

---
+++ src/AT.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect, useMemo } from "react";
+
+function gigFormatter(packages) {
+  return packages.map((pack) => pack + "GB");
+}
+
+function amounts(dictionary, packages) {
+  return packages.map((pack) => dictionary[parseInt(pack)]);
+}
+
+const Home = () => {
+  const initialAgentPrices = useMemo(
+    () => ({
+      1: 4.3,
+      2: 8.5,
+      3: 11.5,
+      4: 15.5,
+      5: 19,
+      6: 22.5,
+      7: 26,
+      8: 29,
+      9: 32,
+      10: 35,
+      11: 4.3 + 35,
+      12: 8.5 + 35,
+      13: 11.5 + 35,
+      14: 15.5 + 35,
+      15: 52,
+      16: 4.3 + 52,
+      17: 8.5 + 52,
+      18: 11.5 + 52,
+      19: 15.5 + 52,
+      20: 68,
+      21: 4.3 + 68,
+      22: 8.5 + 68,
+      23: 11.5 + 68,
+      24: 15.5 + 68,
+      25: 82,
+      26: 4.3 + 82,
+      27: 8.5 + 82,
+      28: 11.5 + 82,
+      29: 15.5 + 82,
+      30: 100,
+      31: 4.3 + 100,
+      32: 8.5 + 100,
+      33: 11.5 + 100,
+      34: 15.5 + 100,
+      35: 117,
+      36: 4.3 + 117,
+      37: 8.5 + 117,
+      38: 11.5 + 117,
+      39: 15.5 + 117,
+      40: 135,
+      41: 4.3 + 135,
+      42: 8.5 + 135,
+      43: 11.5 + 135,
+      44: 15.5 + 135,
+      45: 146,
+      46: 4.3 + 146,
+      47: 8.5 + 146,
+      48: 11.5 + 146,
+      49: 15.5 + 146,
+      50: 158,
+      100: 300,
+    }),
+    []
+  );
+
+  const [inputValue, setInputValue] = useState("");
+  const [inputError, setInputError] = useState("");
+  const [tableContent, setTableContent] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    let values = inputValue.split("+").map((value) => value.trim());
+    let packs = gigFormatter(values);
+    let prices = amounts(initialAgentPrices, values);
+    const formattedTable = tabularFormat(packs, prices);
+    setTableContent(formattedTable);
+  }, [inputValue, initialAgentPrices]);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+
+    const validInputRegex = /^[0-9+\s]*$/;
+
+    if (validInputRegex.test(inputValue)) {
+      setInputValue(inputValue);
+      setInputError("");
+    } else {
+      setInputError("Input must contain only numbers, spaces, and +");
+    }
+  };
+
+  const tabularFormat = (packages, prices) => {
+    return (
+      <div>
+        <h4 className="sales-header">
+          <span>Packs</span>
+          <span>Prices</span>
+        </h4>
+        {packages.map((pack, index) => {
+          const packLen = pack.length;
+          const priceLen = String(prices[index]).length;
+          const indexLen = String(index + 1).length;
+          const totalLen = 20;
+          const dotsLen = totalLen - (packLen + priceLen + indexLen + 5); // 5 is the number of additional characters including dots, spaces, and indexes
+
+          let dots = "";
+          for (let i = 0; i < dotsLen; i++) {
+            dots += ".";
+          }
+
+          return (
+            <p key={index}>
+              {index + 1}. {pack} {dots} {prices[index]}
+            </p>
+          );
+        })}
+        <p>
+          Total: GH&#8373;
+          {prices.reduce((acc, cur) => acc + cur, 0).toFixed(2)}
+        </p>
+        <p>Orders placed on {new Date().toLocaleDateString("en-GB")}</p>
+      </div>
+    );
+  };
+
+  const handleCopyToClipboard = () => {
+    if (inputValue) {
+      let values = inputValue.split("+").map((value) => value.trim());
+      let packs = gigFormatter(values);
+      let prices = amounts(initialAgentPrices, values);
+      const plainTextLines = plainTextFormat(packs, prices);
+
+      const plainText = plainTextLines.join("\n");
+
+      navigator.clipboard
+        .writeText(plainText)
+        .then(() => {
+          setIsCopied(true);
+          setTimeout(() => {
+            setIsCopied(false);
+          }, 1500);
+        })
+        .catch((err) => console.error("Failed to copy:", err));
+    }
+  };
+
+  function plainTextFormat(packages, prices) {
+    let output = [];
+    output.push("\n*PACKS*\t\t*PRICES*");
+    for (let i = 0; i < packages.length; i++) {
+      let pack = packages[i];
+      let price = prices[i];
+      let packLen = pack.length;
+      let priceLen = price.toString().length;
+      let middleLen =
+        30 - (packLen + 1 + (priceLen + 1) + (i.toString().length + 2));
+      let line = `${i + 1}. ${pack}`;
+      for (let j = 0; j < middleLen; j++) {
+        line += ".";
+      }
+      line += ` ${price}`;
+      output.push(line);
+    }
+    let total = prices.reduce((acc, curr) => acc + curr, 0);
+    output.push(`\n*Total: GH₵${total}*`);
+    let today = new Date().toLocaleDateString();
+    output.push(`\n*Orders placed on ${today}*`);
+    return output;
+  }
+
+  return (
+    <div className="main-container">
+      <h3 className="home-header">Daily Sales Calculator</h3>
+
+      <div className="form">
+        <div className="input-sales">
+          <label htmlFor="day_sales">
+            Enter your sales packages separated with +
+          </label>
+          <input
+            type="text"
+            name="sales"
+            id="day_sales"
+            placeholder="10 + 7 + 9 + 6 + 4"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          {inputError && <p>{inputError}</p>}
+        </div>
+      </div>
+      <div className="packs-container form">
+        {tableContent}
+        {!isCopied && (
+          <button className="copy" onClick={handleCopyToClipboard}>
+            Copy
+          </button>
+        )}
+        {isCopied && <p>copied!</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
